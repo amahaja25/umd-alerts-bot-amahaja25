557a10850f3e65f8979fe2139618e74777cf65f2
amahaja25 <135137373+amahaja25@users.noreply.github.com>
2024-03-14 16:42:31 +0000

    updated readme and did some stuff to scraper

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # umd-alerts-bot-amahaja25
 
+3/14 
+* I created the python file for the scraper as well as the csv that i want to store the alerts in.
+* Created some sort of start to the scraper, but there is more I need to work on. Starting out, I was able to get the links to the alerts and attach them to the base UMD Alerts link, get the date of the alert, as well as the title, and write this all out to the csv. 
+* I need to now find a way to scrape the individual alert pages so I can get the text, and further split this up into things like location and time of incident.
+* Need to split up alerts by type -- Gas Line, Robbery, Indecent Exposure, On- vs Off-Campus.
+* Have to implement GitHub Actions so automate running this every minute or two minutes so I can be most up to date on the alerts.
--- scraper.py
+++ scraper.py
@@ -8,27 +8,35 @@ list_of_rows = []
 # this is the url i would like to scrape
 url = 'https://alert.umd.edu/alerts' 
 
-# scraper
 response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0'})
 html = response.content
 
-soup = BeautifulSoup(html, features="html.parser")
-lis = soup.find('li')
+soup = BeautifulSoup(html, features= "html.parser")
 
+# this specifies which specific links i want. without this, i would also end up scraping things like the about and faq page links.
+main = soup.find('ul', {"class": "feed"})
+
+lis = main.find_all('li')
 for li in lis:
     list_of_cells = []
-    anchor = li.find('a')
-    if anchor:
-        link = "https://alert.umd.edu/alerts/" + li.find('a')['href']
+    if li.find('a'):
+        link = 'https://alert.umd.edu/alerts' + li.find('a')['href']
         list_of_cells.append(link)
+    day = li.find('time')
+    if day:
+        date = day.text.strip()
+        list_of_cells.append(date)
     paragraph = li.find('p')
+    # note that this isn't exactly what i want. i have to figure out how to go to the individual alert link and get the text from there.
     if paragraph:
         text = paragraph.text.strip()
         list_of_cells.append(text)
     list_of_rows.append(list_of_cells)
 
+# if i want to also scrape the past alerts, i have to find a way to iterate through all the pages at the bottom. but each page doesn't have a separate url which is annoying so have to deal with that.
+
 # write to the umd_alerts.csv file
 outfile = open("umd_alerts.csv", "w")
 writer = csv.writer(outfile)
-writer.writerow(["url", "text"])
+writer.writerow(["link", "date", "text"])
 writer.writerows(list_of_rows)
